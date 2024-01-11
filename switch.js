e4f9a115a5549b27a253706732cc111182b9946b
titouan <144939456+VERNAGUT-Titouan-2326112-A@users.noreply.github.com>
2024-01-11 11:13:39 +0100

    hhdjghdd

--- switch.js
+++ switch.js
@@ -1,13 +1,24 @@
 let checkbox = document.querySelector("#darkmode-toggle");
 
-checkbox.addEventListener("change", (elem) => {
-	console.log(elem.checked);
+checkbox.addEventListener("change", () => {
 	let baliseStyle = document.querySelector("#colorCssFile");
 
 	if (baliseStyle.getAttribute("href") === "light.css") {
 		baliseStyle.setAttribute("href", "dark.css");
-		return
+		return;
 	}
 
 	baliseStyle.setAttribute("href", "light.css");
+});
+
+let menu = document.querySelector("#openBtn");
+
+menu.addEventListener("click", () => {
+	let nav = document.querySelector("#nav-menu");
+
+	if (nav.classList.contains("nav-hidden")){
+		nav.classList.remove("nav-hidden");
+		return;
+	}
+	nav.classList.add("nav-hidden");
 });
